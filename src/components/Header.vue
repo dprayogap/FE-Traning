6384f757538110f18ca7be371a60c356a04934d2
dimas.prayoga <agyoga123@gmail.com>
2021-03-16 17:07:28 +0700

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app-header">
+        <div class="app-header__container">
+            <div class="app-header__logo">
+
+            </div>
+            <div class="app-header__search-frame">
+
+            </div>
+            <div class="app-header__action-frame">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+
+}
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+
+    &__container {
+        display: flex;
+    }
+
+}
+</style>
